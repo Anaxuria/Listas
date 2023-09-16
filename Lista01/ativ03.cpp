30ec359d85302b4eb7f26296d7924548ff67c1ea
anaxuria <anajuliagarcia.ajg@gmail.com>
2023-09-16 18:59:48 -0300

    add ativ 03 and 04

--- Lista01/ativ03.cpp
+++ Lista01/ativ03.cpp
@@ -5,9 +5,39 @@
 int main() {
 	setlocale(LC_ALL, "portuguese");
 
-	//3)Faça um programa que receba dez números inteiros. Calcule e mostre: 
-    //• A soma dos números primos 
-    //• A média dos números múltiplos de 3 que são maiores que 10 
-    
-	
+	int num10=1, num, k, div=0, soma=0;
+	float total=0, quant_med=0, media=0;
+
+	printf("Digite o numero: ");
+	scanf("%i", &num);
+
+	do {
+
+		div=0;
+
+        for(k=1;k<=num;k++){
+			if(num % k == 0){
+				div++;
+			}
+		}
+
+		if(div == 2){
+			soma+= num;
+		}
+
+		if(num % 3 == 0 && num > 10){
+			total+=num;
+			quant_med++;
+		}
+
+        printf("Digite o numero: ");
+	    scanf("%i", &num);
+
+		num10++;
+	} while(num10 < 10);
+
+	media = total/quant_med;
+
+    printf("\nSoma dos numeros primos: %i", soma);
+	printf("\nQuantidade de numeros multiplos de 3 e maior que 10: %.1f", media);
 }
--- Lista01/ativ04.cpp
+++ Lista01/ativ04.cpp
@@ -5,5 +5,46 @@
 int main() {
 	setlocale(LC_ALL, "portuguese");
 
-    
+	int idade, op, reg=0, bom=0;
+	float med=0, tot_id=0, oti=0;
+
+	printf("Digite sua idade: ");
+	scanf("%i", &idade);
+
+	if(idade < 0){
+		printf("Esta idade nao eh valida. \nPor favor, apenas valores positivos.");
+	} else {
+		while(idade>0){
+
+			printf("Digite sua opniao sobre o filme: (3-otimo; 2-bom; 1-regular) ");
+			scanf("%i", &op);
+
+			switch(op){
+				case 3:
+					tot_id+=idade;
+					oti++;
+					break;
+				case 2:
+					bom++;
+					break;
+				case 1:
+					reg++;
+					break;
+				default:
+					printf("Nota nao valida, tente novamente. \n");
+			}
+
+			printf("\nDigite sua idade: (Digite '0' para parar o programa) ");
+			scanf("%i", &idade);
+		}
+		
+		if(oti > 0){
+			med=tot_id/oti;
+			printf("\nA media das idades dos que votaram em 'otimo' eh: %.1f", med);
+		} else {
+			printf("\nNao tivemos notas 'otimo' para fazer a media.");
+		}
+		printf("\nA quantidade de pessoas que votou em 'bom' eh: %i", bom);
+		printf("\nA quantidade de pessoas que votou em 'regular' eh: %i", reg);
+	}
 }
