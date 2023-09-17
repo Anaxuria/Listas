cc93ef82bc07a9d1a75783dad82bedd5b7d3775c
anaxuria <anajuliagarcia.ajg@gmail.com>
2023-09-17 18:37:28 -0300

    add lista 02 with ativ00 and ativ01

---
+++ Lista02/ativ01.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "portuguese");
+
+    //1)Escreva um programa que leia a idade de 10 pessoas e armazene-as em umvetor.Calcule e mostre:
+    //a) a menor idade
+    //b) a média das idades
+    //c) a quantidade de pessoas que tem idade entre 20 e 30 anos (inclusive)
+    //d) a quantidade de pessoas com idade maior que a média
+}
