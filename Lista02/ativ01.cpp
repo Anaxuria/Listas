e7354fe45f2929ff4f0146dd559f0899bc8210ef
anaxuria <anajuliagarcia.ajg@gmail.com>
2023-10-01 16:30:16 -0300

    add ativ01 lista2

--- Lista02/ativ01.cpp
+++ Lista02/ativ01.cpp
@@ -5,9 +5,38 @@
 int main() {
 	setlocale(LC_ALL, "portuguese");
 
-    //1)Escreva um programa que leia a idade de 10 pessoas e armazene-as em umvetor.Calcule e mostre:
-    //a) a menor idade
-    //b) a média das idades
-    //c) a quantidade de pessoas que tem idade entre 20 e 30 anos (inclusive)
-    //d) a quantidade de pessoas com idade maior que a média
+    int i, menorIdade=999999, idade20e30=0, maiorMedia=0;
+    float idade[10], total, media;
+
+    for(i=0; i<10;i++){
+        printf("Digite a idade: ");
+        scanf("%f", &idade[i]);
+
+        if(idade[i]<menorIdade){
+            menorIdade=idade[i];
+        }
+
+        total += idade[i];
+
+        if(idade[i] >= 20 && idade[i] <= 30){
+            idade20e30++;
+        }
+    }
+
+    media = total/10;
+
+    for(i=0; i<10;i++){
+        if(idade[i]>media){
+            maiorMedia++;
+        }
+    }
+
+    printf("\nA menor idade eh: %i", menorIdade);
+    printf("\nA media das idades eh %.2f", media);
+    printf("\nA quantidade de idades entre 20 e 30 anos eh: %i", idade20e30);
+    if(maiorMedia>0){
+        printf("\nA quantidade de idades maior que a media eh: %i", maiorMedia);
+    }else{
+        printf("\nNão temos idades maiores que a media.");
+    }
 }
