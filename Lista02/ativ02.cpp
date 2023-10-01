777d431958649f607ed0f001cab94e6f0570b9e0
anaxuria <anajuliagarcia.ajg@gmail.com>
2023-10-01 16:31:20 -0300

    add ativ02 lista2

---
+++ Lista02/ativ02.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "portuguese");
+
+    //2)Faça um programa que preencha umvetorcom 10 coresdiferentes. Depois permita fazer uma  pesquisa  se  uma determinadacorexiste  armazenada no  vetor,  se  existir  deve  ser impresso  na  tela a  cor  e  em  qual  posição  (índice)  esta  cor  está armazenada.  Apesquisa deve ser feita até que seja digitado FIM na cor a ser pesquisadana lista.
+}
